355d2710a41713e93c64a211fa4facedc73891c3
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-04-02 21:25:05 -0300

    :tada: initial commit: setting up project folders

---
+++ dev/api/src/main/java/com/henriquecostadev/costumersapi/CostumersapiApplication.java
@@ -0,0 +1,12 @@
+package com.henriquecostadev.costumersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumersapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumersapiApplication.class, args);
+	}
+}
---
+++ dev/api/src/test/java/com/henriquecostadev/costumersapi/CostumersapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquecostadev.costumersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumersapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
